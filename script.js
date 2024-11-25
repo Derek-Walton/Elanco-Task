0a3e82fa04753e8b36a7fd6c9a6c897b9aed677e
Derek Walton <derekwalton19@gmail.com>
2024-11-25 17:51:49 +0000

    Working data inserts and table creates

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const table = document.querySelector('#table');
 
+// API Requests
+
 async function getCountriesAndCapital() {
   const response = await fetch('https://countriesnow.space/api/v0.1/countries/capital');
   if (!response.ok) {
@@ -11,6 +13,8 @@ async function getCountriesAndCapital() {
   return data.data;
 }
 
+// getCountriesAndCapital();
+
 async function getCountriesAndPopulation() {
   const response = await fetch('https://countriesnow.space/api/v0.1/countries/population/cities');
   if (!response.ok) {
@@ -18,13 +22,13 @@ async function getCountriesAndPopulation() {
   }
   const data = await response.json();
 
-  console.log(data.data);
-
   // returns an array of every city and it's population count, country and (not all) the year the count was taken, it's value, it's reliability and if both sex's took part
   return data.data;
 }
 
-async function getFlag(countryName) {
+// getCountriesAndPopulation();
+
+async function getCountryFlag(countryName) {
   const response = await fetch('https://countriesnow.space/api/v0.1/countries/flag/images', {
     // specifies the api that json is being sent through the body
     headers: {
@@ -45,13 +49,98 @@ async function getFlag(countryName) {
   return flagUrl;
 }
 
+// getCountryFlag('nigeria');
 
 
-getCountriesAndCapital();
+// Additional API Request
 
-// getFlag('nigeria');
+async function getCountryPopulation(countryName) {
+  const response = await fetch('https://countriesnow.space/api/v0.1/countries/population', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      'country': countryName
+    })
+  })
 
-// getCountriesAndPopulation();
+  if (!response.ok) {
+    throw new Error(`Error occurred retrieving ${countryName}'s population. Status code: ${response.status}`)
+  }
+  const data = await response.json();
+  const population = data.data.populationCounts;
+  
+  // returns an array of all the population counts for the country (highest index being the newest count)
+  return population;
+}
+
+// getCountryPopulation('nigeria');
+
+
+
+// Creating and Inserting Data to the document
+
+async function createTableRows() {
+  const countries = await getCountriesAndCapital();
+  for (const country of countries) {
+    const tableRow = document.createElement('tr');
+
+    const countryElem = document.createElement('td');
+    countryElem.textContent = emptyFieldCheck(country.name, 'Country Name');
+
+    const flagElem = document.createElement('td');
+    
+    try {
+      const countryFlagUrl = await getCountryFlag(country.name);
+      
+      const flagImg = document.createElement('img');
+
+      flagImg.src = countryFlagUrl;
+      flagImg.alt = `${country}'s Flag`;
+      flagImg.classList.add('flag');
+      flagElem.append(flagImg);
+    } catch (error) {
+      flagElem.textContent = 'N/A';
+    }
+    
+    const totalPopulationElem = document.createElement('td');
+    const totalPopulationYearElem = document.createElement('td');
+
+    try {
+      const countryPopulation = await getCountryPopulation(country.name);
+
+      const newestPopulationValue = countryPopulation[countryPopulation.length - 1].value;
+      const newestPopulationYear = countryPopulation[countryPopulation.length - 1].year;
+    
+      totalPopulationElem.textContent = emptyFieldCheck(newestPopulationValue, 'Population Count');
+      totalPopulationYearElem.textContent = emptyFieldCheck(newestPopulationYear, 'Population Year');
+      
+    } catch (error) {
+      totalPopulationElem.textContent = 'N/A: Missing Population Count';
+      totalPopulationYearElem.textContent = 'N/A: Missing Population Year';
+    }
+
+    const countryCapitalElem = document.createElement('td');
+    countryCapitalElem.textContent = country.capital;
+
+
+
+    tableRow.append(countryElem, flagElem, totalPopulationElem, totalPopulationYearElem, countryCapitalElem);
+    table.append(tableRow);
+  };
+}
+
+// Fills empty cells incase data is missing
+function emptyFieldCheck(name, field = 'data') {
+  if (!name) {
+    return `N/A: Missing ${field}`;
+  }
+  return name
+}
+
+createTableRows();
 
 // table plan
-// country, flag, total population, capital, capital population
+// country, flag, total population, populatoin year, capital
+// country, flag, total population, populatoin year, capital, capital population
